2b00d66c7e9ec36d6beb2671442ab43f69843bfb
chori0204 <chori0204@gmail.com>
2025-08-25 14:52:53 +0900

    잘못 생성된 파일 삭제 후 재반영

--- test/features/detail_page/presentation/bloc/detail_page_bloc_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:bloc_test/bloc_test.dart';
-import 'package:detail_page/presentation/bloc/detail_page_bloc.dart';
-
-void main() {
-  group('UdetailUpageBloc', () {
-    // TODO: bloc 테스트 구현
-  });
-}
--- test/features/home_page/presentation/bloc/home_page_bloc_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:bloc_test/bloc_test.dart';
-import 'package:home_page/presentation/bloc/home_page_bloc.dart';
-
-void main() {
-  group('Bloc', () {
-    // TODO: bloc 테스트 구현
-  });
-}
